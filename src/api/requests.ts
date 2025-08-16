043a18949a327a2b4f7d9d8450fdb1025a611bca
taichoup <taichoup@gmx.net>
2025-08-17 00:02:20 +0200

    wip dedicated file for request making

---
+++ src/api/requests.ts
@@ -0,0 +1,68 @@
+
+import { type components } from '../../shared/types';
+import { Task } from '../components/Task';
+
+const API_GATEWAY_ID = "6s6jd82e80";
+const API_URL = `https://${API_GATEWAY_ID}.execute-api.eu-north-1.amazonaws.com/preprod/tasks`
+
+type Task = components["schemas"]["Task"];
+
+// Fetch tasks from API
+export async function fetchTasks() {
+    const res = await fetch(API_URL, { method: "GET" });
+    if (!res.ok) {
+        console.error("Failed to fetch tasks", res.statusText);
+        return;
+    }
+    const data: Task[] = await res.json();
+    return data;
+}
+
+// Add a task
+// TODO: this should take the frequency as param too. 
+// The type of the param should be <components["schemas"]["NewTask"]
+export async function addTask(newTitle: string) {
+    if (!newTitle.trim()) throw new Error('New title cannot be empty');
+    const res = await fetch(API_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ title: newTitle, frequency: "daily" }), // TODO
+    });
+    if (!res.ok) {
+        throw new Error(`Failed to add task. ${res.statusText}`);
+    }
+    return res;
+}
+
+// Toggle task completion
+export async function toggleTask(task: Task) {
+    const res = await fetch(API_URL, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            id: task.id,
+            checked: !task.checked,
+            lastChecked: !task.checked ? new Date().toISOString() : "",
+        }),
+    });
+    if (!res.ok) {
+        console.error("Failed to update task", res.statusText);
+        return;
+    }
+    await fetchTasks();
+}
+
+export async function deleteTask(id: string) {
+    const deleteUrl = `${API_URL}/${id}`;
+    try {
+        fetch(deleteUrl, {
+            method: "DELETE",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                id,
+            }),
+        });
+    } catch (serverError) {
+        console.error("Could not delete the task");
+    }
+}
